7c94e8c2ea310a8c108585ecc5ad4eb672a0a8eb
Daichi TOMA <daichi.toma@mixi.co.jp>
2015-03-05 21:52:35 +0900

    solve exercises

--- example2.hs
+++ example2.hs
@@ -1,7 +1,9 @@
+import Control.Monad
+import Data.Maybe
 {- Author:     Jeff Newbern
-Maintainer: Jeff Newbern <jnewbern@nomaware.com>
-Time-stamp: <Mon Nov 10 11:58:21 2003>
-License:    GPL
+   Maintainer: Jeff Newbern <jnewbern@nomaware.com>
+   Time-stamp: <Mon Nov 10 11:58:21 2003>
+   License:    GPL
 -}
 
 {- DESCRIPTION
@@ -9,7 +11,7 @@ License:    GPL
 Example 2 - Do notation
 
 Usage: Compile the code and execute the resulting program.
-It will print Dolly's maternal grandfather.
+       It will print Dolly's maternal grandfather.
 -}
 
 -- everything you need to know about sheep
@@ -17,7 +19,7 @@ data Sheep = Sheep {name::String, mother::Maybe Sheep, father::Maybe Sheep}
 
 -- we show sheep by name
 instance Show Sheep where
-    show s = show (name s)
+  show s = show (name s)
 
 -- the Maybe type is already declared as an instance of the Monad class
 -- in the standard prelude, so we don't actually need to define it here.
@@ -31,16 +33,50 @@ instance Show Sheep where
 maternalGrandfather :: Sheep -> Maybe Sheep
 maternalGrandfather s = do m <- mother s
                            father m
+                            
+maternalGrandfather' :: Sheep -> Maybe Sheep
+maternalGrandfather' s = mother s >>= father 
 
 fathersMaternalGrandmother :: Sheep -> Maybe Sheep
 fathersMaternalGrandmother s = do f  <- father s
                                   gm <- mother f
                                   mother gm
 
+fathersMaternalGrandmother' :: Sheep -> Maybe Sheep
+fathersMaternalGrandmother' s = father s >>= \f -> mother f >>= \gm -> mother gm
+
 mothersPaternalGrandfather :: Sheep -> Maybe Sheep
 mothersPaternalGrandfather s = do m  <- mother s
                                   gf <- father m
                                   father gf
+
+mothersPaternalGrandfather' :: Sheep -> Maybe Sheep
+mothersPaternalGrandfather' s = mother s >>= \m -> father m >>= \gf -> father gf
+
+parent :: Sheep -> Maybe Sheep
+parent s = (father s) `mplus` (mother s)
+
+grandparent :: Sheep -> Maybe Sheep
+grandparent s = (mother s >>= parent) `mplus` (father s >>= parent )
+
+parentlist :: Sheep -> [Sheep]
+parentlist s = (maybeToList $ father s) `mplus` (maybeToList $ mother s)
+
+grandparentlist :: Sheep -> [Sheep]
+grandparentlist s = (maybeToList (mother s) >>= parentlist) `mplus` (maybeToList (father s) >>= parentlist)
+
+grandparentlist2 :: Sheep -> [Sheep]
+grandparentlist2 s = parentlist s >>= parentlist
+
+maybeToMonad :: (MonadPlus m) => Maybe a -> m a
+maybeToMonad Nothing  = mzero
+maybeToMonad (Just a) = return a
+
+mparent :: (MonadPlus m) => Sheep -> m Sheep
+mparent s = (maybeToMonad $ father s) `mplus` (maybeToMonad $ mother s)
+
+mgrandparent :: (MonadPlus m) => Sheep -> m Sheep
+mgrandparent s = (maybeToMonad (mother s) >>= mparent) `mplus` (maybeToMonad (father s) >>= mparent )
 
 -- this builds our sheep family tree
 breedSheep :: Sheep
@@ -52,12 +88,17 @@ breedSheep = let adam   = Sheep "Adam" Nothing Nothing
                  holly  = Sheep "Holly" (Just eve) (Just adam)
                  roger  = Sheep "Roger" (Just eve) (Just kronos)
                  molly  = Sheep "Molly" (Just holly) (Just roger)
-                 in Sheep "Dolly" (Just molly) Nothing
+             in Sheep "Dolly" (Just molly) Nothing
 
 -- print Dolly's maternal grandfather
 main :: IO ()
 main = let dolly = breedSheep
-           in do print (maternalGrandfather dolly)
-
+       in do print (maternalGrandfather dolly)
+             print (mparent dolly :: Maybe Sheep) 
+             print (mparent dolly :: [Sheep]) 
+             print (grandparent dolly) 
+             print (grandparentlist dolly) 
+             print (mgrandparent dolly :: Maybe Sheep) 
+             print (mgrandparent dolly :: [Sheep]) 
+  
 -- END OF FILE
-
